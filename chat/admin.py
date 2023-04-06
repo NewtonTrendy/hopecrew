321ebbc1ab2da3e6a66a9f1e4f361b4ccb77f6e3
felix <felix.farquharson@gmail.com>
2023-04-06 14:37:46 +0100

    ui updates

--- chat/admin.py
+++ chat/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from chat.models import Message, Command, CommandInput, UserPing
+from chat.models import Message, Command, CommandInput, UserPing, Report
 
 # Register your models here.
 admin.site.register(Message)
 admin.site.register(UserPing)
 admin.site.register(Command)
 admin.site.register(CommandInput)
+admin.site.register(Report)
--- main/views.py
+++ main/views.py
@@ -1,6 +1,4 @@
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
-from django.shortcuts import render
 from django.views.generic import CreateView
 from main.models import Contact
 
